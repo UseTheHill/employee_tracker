dba10e9bddbcd956d8cad7b9c76130edbb198564
Kayte McDonough <kaytemcdonough@gmail.com>
2021-04-17 15:54:29 -0400

    viewEmployees

--- index.js
+++ index.js
@@ -45,3 +45,20 @@ const start = () => {
         }
       });
   };
+
+const viewEmployees = () => {
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;`;
+
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log("\n");
+        console.table(res);
+        start();
+      });
+    
+};
