ccd635ee8a29ea28155fb24de8518929823c5679
Kayte McDonough <kaytemcdonough@gmail.com>
2021-04-17 15:48:48 -0400

    const start

--- index.js
+++ index.js
@@ -19,13 +19,29 @@ connection.connect((err) => {
 
 const start = () => {
     inquirer
-    .prompt({
+      .prompt({
         name: "startOptions",
         type: "list",
-        message: "What would you like to do?"
+        message: "What would you like to do?",
         choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "Exit"],
-    })
-    .then((answer)) => {
-        
-    }
-}
+      })
+      .then((answer) => {
+        if (answer.startOptions === "View All Employees") {
+          viewEmployees();
+        } else if (answer.startOptions === "View All Employees by Department") {
+          viewDepartment();
+        } else if (answer.startOptions === "View All Employees by Manager") {
+          viewManager();
+        } else if (answer.startOptions === "Add Employee") {
+          addEmployee();
+        } else if (answer.startOptions === "Remove Employee") {
+          removeEmployee();
+        } else if (answer.startOptions === "Update Employee Role") {
+          updateRole();
+        } else if (answer.startOptions === "Update Employee Manager") {
+          updateManager();
+        } else if (answer.startOptions === "Exit") {
+          connection.end();
+        }
+      });
+  };
