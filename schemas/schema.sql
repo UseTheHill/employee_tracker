9795fb2939642daae23e6d3a9d4955c534de326b
Kayte McDonough <kaytemcdonough@gmail.com>
2021-04-17 16:08:45 -0400

    edited seed

--- schemas/schema.sql
+++ schemas/schema.sql
@@ -1,42 +1,32 @@
-DROP DATABASE IF EXISTS employee_mgmt_db;
-CREATE DATABASE employee_mgmt_db;
-USE employee_mgmt_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
 
-CREATE TABLE departments (
+CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(30),
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
-INSERT INTO departments (department_name) values ('Sales'), ('Engineering'), ('Finance'), ('Management');
-
-CREATE TABLE roles (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL(10,2),
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id),
-    KEY fk_deparmtnet_id (department_id),
-    CONSTRAINT fk_department_id FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT fk_department_id
+    FOREIGN KEY (department_id)
+    REFERENCES department(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    PRIMARY KEY (id)
 );
 
-INSERT INTO roles (title, salary, department_id) values ("Web Developer", "80000", 2)
-
-INSERT INTO roles (title, salary, department_id) values ("Software Engineer", "100000", 2)
-
-CREATE TABLE employees (
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR (30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NULL,
-    PRIMARY KEY (id),
-    KEY fk_role_id (role_id),
-    CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE RESTRICT ON UPDATE CASCADE, 
-    KEY fk_department_id (manager_id),
-    CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employees(id)
+    manager_id INT NULL, 
+    CONSTRAINT fk_role_id
+    FOREIGN KEY (role_id)
+    REFERENCES role(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    PRIMARY KEY (id)
 );
-
-INSERT INTO employees (first_name, last_name, role_id, manager_id) values ("Kayte", "McDonough", 1, 3);
-
-INSERT INTO employees (first_name, last_name, role_id) values ("Caroline", "Felice", 2);
