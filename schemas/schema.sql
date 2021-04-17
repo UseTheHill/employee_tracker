be4f82493d8325686b15124afa8ac96aa3e6d407
Kayte McDonough <kaytemcdonough@gmail.com>
2021-04-17 14:33:02 -0400

    added more to schema

--- schemas/schema.sql
+++ schemas/schema.sql
@@ -8,4 +8,20 @@ CREATE TABLE departments (
     PRIMARY KEY (id)
 );
 
+INSERT INTO departments (department_name) values ('Sales'), ('Engineering'), ('Finance'), ('Management');
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    KEY fk_deparmtnet_id (department_id),
+    CONSTRAINT fk_department_id FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+INSERT INTO roles (title, salary, department_id) values ("Web Developer", "80000", 2)
+
+INSERT INTO roles (title, salary, department_id) values ("Software Engineer", "100000", 2)
+
 
