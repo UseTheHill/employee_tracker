4421cb5140bba589b0ef1f135b3eedde2238cedf
Kayte McDonough <kaytemcdonough@gmail.com>
2021-04-17 14:56:21 -0400

    added connection to index.js

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const inquirer = require ("inquirer");
 const consoleTable = require('console.table');
 require("dotenv").config();
 
-const {}
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DATABASE,
+});
