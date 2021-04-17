5839b28460d270f78e3c62e350a1381673716bb8
Kayte McDonough <kaytemcdonough@gmail.com>
2021-04-17 14:41:19 -0400

    finished schema

--- schemas/schema.sql
+++ schemas/schema.sql
@@ -36,3 +36,7 @@ CREATE TABLE employees (
     KEY fk_department_id (manager_id),
     CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) values ("Kayte", "McDonough", 1, 3);
+
+INSERT INTO employees (first_name, last_name, role_id) values ("Caroline", "Felice", 2);
