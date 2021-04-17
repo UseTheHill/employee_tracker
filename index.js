c8723ce39ab1824be3e2ac5a7fccd8b5a9aa599d
Kayte McDonough <kaytemcdonough@gmail.com>
2021-04-17 16:36:56 -0400

    vewManager

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ const viewEmployees = () => {
     const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
     FROM employee
     LEFT JOIN employee manager on manager.id = employee.manager_id
-    INNER JOIN role ON (role.id = employee.role_id)
-    INNER JOIN department ON (department.id = role.department_id)
+    INNER JOIN roles ON (roles.id = employee.roles_id)
+    INNER JOIN department ON (department.id = roles.department_id)
     ORDER BY employee.id;`;
 
     connection.query(query, (err, res) => {
@@ -65,9 +65,25 @@ const viewEmployees = () => {
 const viewDepartment = () => {
     const query = `SELECT departmnet.name AS department, role.title, employee.id, employee.first_name, employee.last_name
     FROM employee
-    LEFT JOIN role ON (role.id = employee.role_id)
-    LEFT JOIN department ON (department.id = role.department_id)
+    LEFT JOIN roles ON (roles.id = employee.roles_id)
+    LEFT JOIN department ON (department.id = roles.department_id)
     ORDER BY department.name;`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log("\n");
+        console.table(res);
+        start();
+    });
+};
+
+const viewManager = () => {
+    const query = `SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager, department.name AS department, employee.id, employee.first_name, employee.last_name, roles.title
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN roles ON (roles.id = employee.roles_id && employee.manager_id != 'NULL')
+    INNER JOIN department ON (department.id = roles.department_id)
+    ORDER BY manager;`;
+
     connection.query(query, (err, res) => {
         if (err) throw err;
         console.log("\n");
